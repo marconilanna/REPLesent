8349763cd028d8481395067c6f8bf766ba7e6dd6
Marconi Lanna <marconi.lanna@gmail.com>
2015-02-08 11:46:56 -0500

    Refactoring: move constants to Config class

--- REPLesent.scala
+++ REPLesent.scala
@@ -47,6 +47,23 @@ case class REPLesent(width: Int = 0, height: Int = 0, input: String = "REPLesent
         (screenWidth, screenHeight)
       }
     }
+
+    private def fill(s: String): String = if (s.isEmpty) s else {
+      val t = s * (screenWidth / s.length)
+      t + s.take(screenWidth - t.length)
+    }
+
+    val topRow = fill(top) + newline
+    val bottomRow = fill(bottom)
+
+    val verticalSpace = screenHeight - 3 // accounts for header, footer, and REPL prompt
+    val horizontalSpace = screenWidth - sinistral.length - dextral.length
+
+    val whiteSpace = " "
+
+    val blankLine = sinistral + {
+      if (dextral.isEmpty) "" else whiteSpace * horizontalSpace + dextral
+    } + newline
   }
 
   // For slides that are part of a build, `size` and `maxLength` refer to the dimensions
@@ -122,43 +139,29 @@ case class REPLesent(width: Int = 0, height: Int = 0, input: String = "REPLesent
   private def render(slide: Slide): String = {
     import config._
 
-    def fill(s: String): String = if (s.isEmpty) s else {
-      val t = s * (screenWidth / s.length)
-      t + s.take(screenWidth - t.length)
-    }
-
-    val separator = " "
-
-    val verticalSpace = screenHeight - 3 // accounts for header, footer, and REPL prompt
-    val horizontalSpace = screenWidth - sinistral.length - dextral.length
-
     val topPadding = (verticalSpace - slide.size) / 2
     val bottomPadding = verticalSpace - topPadding - slide.content.size
 
-    val leftPadding = separator * ((horizontalSpace - slide.maxLength) / 2)
-
-    val blank = sinistral + {
-      if (dextral.isEmpty) "" else separator * horizontalSpace + dextral
-    } + newline
+    val leftPadding = whiteSpace * ((horizontalSpace - slide.maxLength) / 2)
 
     val sb = StringBuilder.newBuilder
 
-    sb ++= fill(top) + newline
-    sb ++= blank * topPadding
+    sb ++= topRow
+    sb ++= blankLine * topPadding
 
     slide.content foreach { line =>
       sb ++= sinistral + leftPadding + line
 
       if (dextral.nonEmpty) {
         val rightPadding = horizontalSpace - leftPadding.length - line.length
-        sb ++= separator * rightPadding + dextral
+        sb ++= whiteSpace * rightPadding + dextral
       }
 
       sb ++= newline
     }
 
-    sb ++= blank * bottomPadding
-    sb ++= fill(bottom)
+    sb ++= blankLine * bottomPadding
+    sb ++= bottomRow
 
     sb.mkString
   }
